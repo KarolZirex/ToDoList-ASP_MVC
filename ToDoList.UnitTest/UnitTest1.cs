2fbe519e2c68faf952db49da036394e82270ef4e
Karol Dura <karol.dura1@wp.pl>
2021-03-04 19:43:44 +0100

    ToDoList project

---
+++ ToDoList.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ToDoList;
+
+namespace ToDoList.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void 
+        {
+
+        }
+    }
+}
